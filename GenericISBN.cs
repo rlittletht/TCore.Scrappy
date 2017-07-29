3978b3fa43113185d93281da9467f1a3512e11be
rlittletht <rlittle@thetasoft.com>
2017-07-29 15:47:32 -0700

    Moved code from UpcInv service code into liblet

---
+++ GenericISBN.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using HtmlAgilityPack;
+using ScrapySharp.Network;
+
+namespace TCore.Scrappy
+{
+    public class GenericISBN
+    {
+        static string sRequestTemplate = "http://isbndb.com/api/books.xml?access_key={0}&index1=isbn&value1={1}";
+
+        public static string FetchTitleFromISBN13(string sCode, string sIsbnDbAccessKey)
+        {
+            string sTitle = "!!NO TITLE FOUND";
+            string sReq = String.Format(sRequestTemplate, sIsbnDbAccessKey, sCode);
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(sReq);
+            if (req != null)
+                {
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+                if (resp != null)
+                    {
+                    Stream stm = resp.GetResponseStream();
+                    if (stm != null)
+                        {
+                        System.Xml.XmlDocument dom = new System.Xml.XmlDocument();
+
+                        try
+                            {
+                            dom.Load(stm);
+
+                            XmlNode node = dom.SelectSingleNode("/ISBNdb/BookList/BookData/Title");
+                            if (node == null)
+                                {
+                                // try again scraping from bn.com...this is notoriously fragile, so its our last resort.
+                                sTitle = "!!NO TITLE FOUND" + sReq + dom.InnerXml; // SScrapeISBN(sIsbn);
+                                }
+                            else
+                                {
+                                sTitle = node.InnerText;
+                                }
+                            }
+                        catch (Exception exc)
+                            {
+                            sTitle = "!!NO TITLE FOUND: (" + exc.Message + ")";
+                            }
+                        }
+                    }
+                }
+
+            return sTitle;
+
+        }
+    }
+
+}
---
+++ GenericUPC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScrapySharp.Network;
+
+namespace TCore.Scrappy
+{
+    public class GenericUPC
+    {
+        public static string FetchTitleFromUPC(string sCode)
+        {
+            if (sCode.Length == 13)
+                sCode = sCode.Substring(1);
+
+            try
+                {
+                ScrapingBrowser sbr = new ScrapingBrowser();
+                sbr.AllowAutoRedirect = false;
+                sbr.AllowMetaRedirect = false;
+
+                WebPage wp = sbr.NavigateToPage(new Uri("http://www.searchupc.com/upc/" + sCode));
+
+                HtmlNodeCollection nodes = wp.Html.SelectNodes("//table[@id='searchresultdata']");
+                HtmlNodeCollection nodesTr = nodes[0].SelectNodes("tr");
+
+                if (nodesTr == null || nodesTr.Count != 2)
+                    {
+                    return "!!NO TITLE FOUND";
+                    }
+                return nodesTr[1].ChildNodes[1].InnerText;
+
+                }
+            catch
+                {
+                return "!!NO TITLE FOUND";
+                }
+        }
+    }
+}
