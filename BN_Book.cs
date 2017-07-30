41d67fa8083051fa87797309b3b712487865c88d
rlittletht <rlittle@thetasoft.com>
2017-07-30 15:39:06 -0700

    add BN book start

---
+++ BN_Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.HtmlControls;
+using System.Xml;
+using HtmlAgilityPack;
+using ScrapySharp.Network;
+
+namespace TCore.Scrappy.BarnesAndNoble
+{
+    public class Book
+    {
+        
+    }
+}
