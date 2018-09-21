c0a1885ee53f2075be346992f695780735d1ec97
Rob Little <rlittletht@users.noreply.github.com>
2018-09-20 19:29:48 -0700

    add generic ISBN implementation

--- GenericISBN.cs
+++ GenericISBN.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Xml;
 using HtmlAgilityPack;
 using ScrapySharp.Network;
@@ -13,50 +14,87 @@ namespace TCore.Scrappy
 {
     public class GenericISBN
     {
-        static string sRequestTemplate = "http://isbndb.com/api/books.xml?access_key={0}&index1=isbn&value1={1}";
+        //static string sRequestTemplate = "http://isbndb.com/api/books.xml?access_key={0}&index1=isbn&value1={1}";
+        static string sRequestTemplate = "https://api.isbndb.com/book/{0}";
+
+        class ISBNBook
+        {
+            public string publisher { get; set; }
+            public string language { get; set; }
+            public string overview { get; set; }
+            public string title_long { get; set; }
+            public string dimensions { get; set; }
+            public string dewey_decimal { get; set; }
+            public string[] subjects { get; set; }
+            public string[] authors { get; set; }
+            public string title { get; set; }
+            public string isbn13 { get; set; }
+            public string isbn { get; set; }
+        }
+
+        class ISBNQueryResponse
+        {
+            public ISBNBook book { get; set; }
+        }
 
         public static string FetchTitleFromISBN13(string sCode, string sIsbnDbAccessKey)
         {
             string sTitle = "!!NO TITLE FOUND";
-            string sReq = String.Format(sRequestTemplate, sIsbnDbAccessKey, sCode);
+            string sReq = String.Format(sRequestTemplate, sCode);
 
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(sReq);
+            HttpWebRequest req = (HttpWebRequest) WebRequest.Create(sReq);
+            req.Headers.Add(String.Format("x-api-key: {0}", sIsbnDbAccessKey));
+
             if (req != null)
                 {
-                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                HttpWebResponse resp;
+
+                try
+                    {
+                    resp = (HttpWebResponse) req.GetResponse();
+                    }
+                catch (Exception exc)
+                    {
+                    sTitle = String.Format("!!NO TITLE FOUND: Exception: {0}", exc.Message);
+                    resp = null;
+                    }
 
                 if (resp != null)
                     {
                     Stream stm = resp.GetResponseStream();
-                    if (stm != null)
+
+                    try
                         {
-                        System.Xml.XmlDocument dom = new System.Xml.XmlDocument();
-
-                        try
+                        if (stm != null)
                             {
-                            dom.Load(stm);
+                            StreamReader stmr = new StreamReader(resp.GetResponseStream());
+                            string sJson = stmr.ReadToEnd();
+                            stmr.Close();
+                            stm.Close();
 
-                            XmlNode node = dom.SelectSingleNode("/ISBNdb/BookList/BookData/Title");
-                            if (node == null)
+                            JavaScriptSerializer jscript = new JavaScriptSerializer();
+
+                            ISBNQueryResponse qr = jscript.Deserialize<ISBNQueryResponse>(sJson);
+
+                            if (qr == null || qr.book.title == null)
                                 {
                                 // try again scraping from bn.com...this is notoriously fragile, so its our last resort.
-                                sTitle = "!!NO TITLE FOUND" + sReq + dom.InnerXml; // SScrapeISBN(sIsbn);
+                                sTitle = "!!NO TITLE FOUND" + sReq + ":" + sJson;
                                 }
                             else
                                 {
-                                sTitle = node.InnerText;
+                                sTitle = qr.book.title;
                                 }
                             }
-                        catch (Exception exc)
-                            {
-                            sTitle = "!!NO TITLE FOUND: (" + exc.Message + ")";
-                            }
+                        }
+                    catch (Exception exc)
+                        {
+                        sTitle = "!!NO TITLE FOUND: (" + exc.Message + ")";
                         }
                     }
                 }
 
             return sTitle;
-
         }
     }
 
